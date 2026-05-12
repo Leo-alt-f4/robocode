5fd207ae4eaaf42d4dc4eb4eaec7d5c462cc6682
Leonardo Cunha <leocunha.cabral@gmail.com>
2026-05-12 19:33:00 -0300

    Put comments into the code

--- Trojan.java
+++ Trojan.java
@@ -1,6 +1,10 @@
 import robocode.*;
 import java.awt.Color;
-
+/*
+    Se acharem relevante alterar o nome, coloca aqui o 
+    nome anterior para ver se tá melhor o de agr ou de antes
+    apoio colocar como 'CavaloDeTroiaDaSilva'
+*/
 public class Trojan extends AdvancedRobot {
     int moveDirection = 1;
 
@@ -14,11 +18,16 @@ public class Trojan extends AdvancedRobot {
         setAdjustGunForRobotTurn(true);
 
         while(true) {
-            turnRadarRight(360);
+            turnRadarRight(360); // sempre que ele nao ver um robo, ele vai ficar rodando até achar
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
+        /* 
+            Com o robo na area do scanner, ele vai ficar rondando ao redor do robo fazendo movimento giratorio
+
+            acho uma boa no futuro deixar mais randomizado isso, vai ser melhor
+        */
         double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
         setTurnRadarRightRadians(robocode.util.Utils.normalRelativeAngle(radarTurn));
 
@@ -26,6 +35,10 @@ public class Trojan extends AdvancedRobot {
 
         setAhead(100 * moveDirection);
 
+        /* 
+            se a distancia baseada no raio de scan for menor que 2, ele atira o fodão, senão é o fraquinho
+            tem que dar um jeito de melhorar o sistema de tiro com mais probabilidades
+        */
         if (e.getDistance() < 200) {
             fire(3);
         } else {
@@ -36,6 +49,9 @@ public class Trojan extends AdvancedRobot {
         setTurnGunRightRadians(robocode.util.Utils.normalRelativeAngle(gunTurn));
     }
 
+    /*
+        tudo isso aq embaixo eh so pra caso do robo bater em algo ou algm, ent isso pode continuar assim (acho eu)
+    */
     public void onHitWall(HitWallEvent e) {
         reverseDirection();
     }
